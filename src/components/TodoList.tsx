90fa981e234cb1cd53bd8512ab32c4ceec9610d8
vipo_1 <vipo_1>
2026-08-12 17:01:09 +0300

    убрал лог

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,7 +14,6 @@ type Props = {
 
 export default function TodoList({setFilter, filter, TodoInfo, tasks, getLoadData}:Props){
 
-  console.log(tasks)
   async function handleDeleteTask(id: number) {
     try{
         await deleteTask(id);
