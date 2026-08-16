998e5a68c00d2555d22496b47ec44b4d1ed31fa2
Vladislav Morozov <m_uladzislau2000@mail.ru>
2026-08-16 04:53:30 +0300

    добавил стриктмод

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
